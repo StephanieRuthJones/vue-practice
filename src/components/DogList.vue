c55b24030a5f3b4fa51d3867803bbec88ed12bf6
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-10-08 11:08:07 -0600

    Added components and event

---
+++ src/components/DogList.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="dog in dogs" :key="dog.id">
+      <DogListing @click.native="upvoteDog(dog.id)" :dog="dog" />
+    </li>
+  </ul>
+</template>
+
+<script>
+import DogListing from "@/components/DogListing";
+export default {
+  components: {
+    DogListing
+  },
+  props: {
+    dogs: Array
+  },
+  methods: {
+      upvoteDog(id){
+          
+      }
+  }
+};
+</script>
+
+<style lang="scss">
+.dogList {
+  ul {
+    list-style: none;
+  }
+}
+</style>
